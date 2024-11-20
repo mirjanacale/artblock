a9cf70b8f8ade1840ab8f922823f2fd7b23dbb23
Mirjana Cale <mirjana083@gmail.com>
2024-11-20 22:17:34 +0000

    Add form in the back end

--- codestar/views.py
+++ codestar/views.py
@@ -4,5 +4,13 @@ from block.models import Post
 
 def details(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+    else:
+        form = CommentForm()
+    
     return render(request, 'core/details.html', context={'post': post, 'form': form})
