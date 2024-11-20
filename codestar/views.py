d06e1a4371a76dac1f1afb03700c98c7aa288851
Mirjana Cale <mirjana083@gmail.com>
2024-11-20 20:52:13 +0000

    fixing details.html due to not opening comments

--- codestar/views.py
+++ codestar/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from codestar.models import CommentForm
-
+from block.forms import CommentForm
 from block.models import Post
 
 def details(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    
     form = CommentForm()
     return render(request, 'core/details.html', context={'post': post, 'form': form})
