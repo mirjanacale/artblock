1519fabde9f7f35a018e03e3945f816ac8daa2c4
Mirjana Cale <mirjana083@gmail.com>
2024-11-21 20:50:30 +0000

    Add class postadmin search_fileds, list_display, list_filter

--- block/admin.py
+++ block/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Post,Category
 
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content'] 
+    list_display = ('title', 'slug','category', 'created_on')
+    list_filter = ('category', 'created_on')
+        
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
--- block/urls.py
+++ block/urls.py
@@ -3,13 +3,7 @@ from codestar import views
 
 
 urlpatterns = [
-
-    path('<slug:category_slug>/<slug:slug>/', views.details, name='post_details'),
     path('<slug:category_slug>/', views.category, name='category_details'),
-]
-
- 
-
-
-
-
+    path('<slug:category_slug>/<slug:slug>/', views.details, name='post_details'),
+    
+   ]
