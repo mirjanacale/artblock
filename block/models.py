2b99733e248eb79346e2a452e4608626e44773dc
Mirjana Cale <mirjana083@gmail.com>
2024-11-23 11:59:02 +0000

    Add API Keys for Clodinary and conected to Heroku

--- block/models.py
+++ block/models.py
@@ -4,6 +4,15 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+class About(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    profile_image = CloudinaryField('image', default='placeholder')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
 class Category(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
